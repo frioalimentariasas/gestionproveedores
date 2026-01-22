1d6319a5dc0f87dd8174c7ef47960d79cb530dc9
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2026-01-22 16:23:03 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,15 +1,8 @@
-// This file is used to initialize the Firebase app.
-// It is used by the `initializeFirebase` function in `src/firebase/index.ts`.
-// It is important to not use this file directly in your components.
-// Instead, use the `useFirebaseApp` hook from `src/firebase/provider.tsx`.
-
-// The environment variables are set in the `.env.local` file.
-// https://firebase.google.com/docs/web/learn-more#config-object
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "proveedores-fal",
+  "appId": "1:215160837890:web:75d017aa2478e7f6eb541b",
+  "apiKey": "AIzaSyB5tuxk4WjpFrPy5LMwhKfy7L6dfUzrVTI",
+  "authDomain": "proveedores-fal.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "215160837890"
 };
