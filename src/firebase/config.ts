9b8e43bf34949da6952f869296f75e3e659a2cb9
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2026-01-27 15:31:30 +0000

    cuando intento Guardar y Bloquear un Formulario me sale el siguiente err

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:215160837890:web:75d017aa2478e7f6eb541b",
   "apiKey": "AIzaSyB5tuxk4WjpFrPy5LMwhKfy7L6dfUzrVTI",
   "authDomain": "proveedores-fal.firebaseapp.com",
+  "storageBucket": "proveedores-fal.appspot.com",
   "measurementId": "",
   "messagingSenderId": "215160837890"
 };
