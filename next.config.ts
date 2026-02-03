18ade0c42297e956ccc1a4706dbc7c04a3adc5f2
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2026-02-03 23:29:47 +0000

    Import trace:   Server Component: ./node_modules/@getbrevo/brevo/src/ind

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['@getbrevo/brevo'],
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
