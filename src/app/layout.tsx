bda351e0dc826f9ec0d631c16faeae498c6a78f1
Frio Alimentaria SAS <app.frioalimentaria@gmail.com>
2026-02-13 03:22:49 +0000

    necesito que SVG que dibujamos se parezca más al que te comparto en la i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ const fontSans = Inter({
   variable: '--font-sans',
 });
 
-// Using an inline SVG data URI to force browsers to ignore cached favicons.
-const faviconHref = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>❄️</text></svg>";
+// Using a custom, multi-layered SVG data URI to create a glow effect and force browsers to ignore cached favicons.
+const faviconHref = "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><defs><filter id='glow'><feGaussianBlur stdDeviation='2.5' /></filter><g id='arm_blue'><path d='M 0 -38 L 0 0' stroke='%2300BFFF' stroke-width='9' stroke-linecap='round'/><path d='M 0 -38 L -7 -31 M 0 -38 L 7 -31' stroke='%2300BFFF' stroke-width='8' stroke-linecap='round'/><path d='M -10 -15 L 10 -15' stroke='%2300BFFF' stroke-width='6' stroke-linecap='round'/><path d='M -12 -25 L 12 -25' stroke='%2300BFFF' stroke-width='7' stroke-linecap='round'/></g><g id='arm_white'><path d='M 0 -38 L 0 0' stroke='white' stroke-width='5' stroke-linecap='round'/><path d='M 0 -38 L -6 -32 M 0 -38 L 6 -32' stroke='white' stroke-width='4' stroke-linecap='round'/><path d='M -8 -15 L 8 -15' stroke='white' stroke-width='3' stroke-linecap='round'/><path d='M -10 -25 L 10 -25' stroke='white' stroke-width='4' stroke-linecap='round'/></g></defs><g transform='translate(50,50)' filter='url(%23glow)'><use href='%23arm_blue' /><use href='%23arm_blue' transform='rotate(60)' /><use href='%23arm_blue' transform='rotate(120)' /><use href='%23arm_blue' transform='rotate(180)' /><use href='%23arm_blue' transform='rotate(240)' /><use href='%23arm_blue' transform='rotate(300)' /></g><g transform='translate(50,50)'><use href='%23arm_white' /><use href='%23arm_white' transform='rotate(60)' /><use href='%23arm_white' transform='rotate(120)' /><use href='%23arm_white' transform='rotate(180)' /><use href='%23arm_white' transform='rotate(240)' /><use href='%23arm_white' transform='rotate(300)' /></g></svg>";
 
 
 export const metadata: Metadata = {
